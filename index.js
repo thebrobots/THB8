7261e87cb38ef1c26b66ce225bba4977fabfb1df
elttayman <77508640+elttayman@users.noreply.github.com>
2021-03-18 21:01:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const servers = client.guilds.cache.size
  
 fetch('https://top.gg/api//bots/800074066949832714/stats', {
         method: 'post',
-        server_count: JSON.stringify(servers),
+        body: JSON.stringify("server_count": servers),
         headers: { "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgwMDA3NDA2Njk0OTgzMjcxNCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjE2MDk2NjgyfQ.xPq_ioJ14Xj2rvOfl-9qYxoDnUznBQ_ZYQMojFqw4PE" }
 
     })
