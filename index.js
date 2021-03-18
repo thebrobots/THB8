9d598b735f28dbcba28e39567b5af35c083c8eae
elttayman <77508640+elttayman@users.noreply.github.com>
2021-03-18 20:08:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Collection } = require("discord.js");
 const client = new Client({ disableMentions: "everyone" });
 const { loadCommands } = require("./commandLoader");
 const { TOKEN, PREFIX } = require("./util/sharkyUtil");
-const express = require("express");
+const fetch = require("node-fetch");
 
 require("./util/eventLoader")(client);
 require("./util/nqn")(client);
