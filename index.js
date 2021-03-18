61ca74db928a4bcdb1287693a544b3ec1a7a7aff
elttayman <77508640+elttayman@users.noreply.github.com>
2021-03-18 20:01:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,32 +8,15 @@ require("./util/eventLoader")(client);
 require("./util/nqn")(client);
 require("./util/muteHelper")(client);
 
-const app = express();
-const cors = require("cors");
-const port = 8080;
-const chalk = require("chalk");
 
-app.use(cors());
-
-app.get("/bot/guilds", (req, res) => {
-  let guild = client.guilds.cache.size;
-  res.json(guild);
-});
-
-app.get("/bot/users", (req, res) => {
-  let members = client.users.cache.size;
-  res.json(members);
-});
-
-app.get("/bot/channels", (req, res) => {
-  let channels = client.channels.cache.size;
-  res.json(channels);
-});
-app.listen(port, () => {
-  console.log(
-    chalk.bgBlueBright.black(` YHF8 is listening to http://localhost:${port} `)
-  );
-});
+ 
+fetch('https://top.gg/api//bots/800074066949832714/stats', {
+        method: 'post',
+        server_count: client.guilds.cache.size,
+ 
+    })
+    .then(res => res.json())
+    .then(json => console.log(json));
 
 client.login(TOKEN);
 client.commands = new Collection();
