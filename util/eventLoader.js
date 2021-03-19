e08b3da191ba37dd759e38801df1f5365694b15d
elttayman <77508640+elttayman@users.noreply.github.com>
2021-03-20 00:03:25 +0100

    Add files via upload

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -4,6 +4,8 @@ module.exports = (client) => {
   const cooldowns = new Discord.Collection();
   client.on("ready", () => reqEvent("ready")(client));
   client.on("message", (m) => reqEvent("message")(m, cooldowns));
+  client.on("message", reqEvent("afk"));
   client.on("guildCreate", reqEvent("guildCreate"));
   client.on("guildDelete", reqEvent("guildDelete"));
+  client.on("voiceStateUpdate", reqEvent("voiceStateUpdate"));
 };
