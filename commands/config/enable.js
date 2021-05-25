dc8555ad65b041656cdf50e888c7af34c74d28eb
elttayman <thettayman@gmail.com>
2021-05-25 16:11:01 +0200

    Update enable.js

--- commands/config/enable.js
+++ commands/config/enable.js
@@ -8,7 +8,7 @@ module.exports = {
   subcommands: "channel, command, logs",
 
   async execute(client, message, args) {
-    if (!message.member.hasPermission("MANAGE_SERVER"))
+    if (!message.member.permissions.has("MANAGE_SERVER"))
       return message.channel.send(
         "Eeeh wait! You can't use that command <a:sh_perms:799392392654225408>"
       );
