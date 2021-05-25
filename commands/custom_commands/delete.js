1b1c29c64d70f7204bd920918706878322732b63
elttayman <thettayman@gmail.com>
2021-05-25 16:11:36 +0200

    Update delete.js

--- commands/custom_commands/delete.js
+++ commands/custom_commands/delete.js
@@ -4,7 +4,7 @@ module.exports = {
   name: "cc-delete",
   aliases: ["ccd"],
   async execute(client, message, args) {
-    if (!message.member.hasPermission("MANAGE_SERVER"))
+    if (!message.member.permissions.has("MANAGE_SERVER"))
       return message.channel.send(
         "Eeeh wait! You can't use that command <a:sh_perms:799392392654225408>"
       );
