54b6e5f153361894eb13f525a8cdf92963a46fe9
elttayman <thettayman@gmail.com>
2021-05-25 16:10:47 +0200

    Update prefix.js

--- commands/config/prefix.js
+++ commands/config/prefix.js
@@ -5,7 +5,7 @@ module.exports = {
   aliases: ["setprefix"],
   Description: "Change the server's prefix",
   async execute(client, message, args) {
-    if (!message.member.hasPermission("MANAGE_SERVER"))
+    if (!message.member.permissions.has("MANAGE_SERVER"))
       return message.channel.send(
         "Eeeh wait! You can't use that command <a:sh_perms:799392392654225408>"
       );
