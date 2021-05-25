f8918afb12ba529c70d19bf0fa952aa013349636
elttayman <thettayman@gmail.com>
2021-05-25 15:53:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const { Client, Collection } = require("discord.js");
-const client = new Client({ disableMentions: "everyone" });
+const { Client, Collection, Intents} = require("discord.js");
+const client = new Client({ 
+    allowedMentions: { parse: ["users", "roles"] },
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_PRESENCES]
+});
+
 const { loadCommands } = require("./loadcmds");
 const { TOKEN, PREFIX } = require("./secured");
 
